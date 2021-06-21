66136d285436faa292b32a137c19c8ccc3723265
monte <montenegrojds@gmail.com>
2021-06-21 11:26:35 -0500

    testing the views and the url

--- booklibrary/settings.py
+++ booklibrary/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #My Apps
-    'api.apps.APIConfig',
+    'api.apps.ApiConfig',
     #Rest Framework
     'rest_framework',
 ]
