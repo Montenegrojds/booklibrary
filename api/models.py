5a4140272cc65e09fa3cd5eaad2f1a1416323677
monte <montenegrojds@gmail.com>
2021-06-21 11:58:25 -0500

    Book models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+Book model:
+- Title (string)
+- Description (string)
+- Author (string)
+- Date of publication (date)
+- Archived (boolean)
+- Stock (integer)
+"""
+class Book(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=400)
+    author = models.CharField(max_length=30)
+    publication_date = models.DateTimeField(auto_now_add=False)
+    archived = models.BooleanField()
+    stock = models.IntegerField(null=False)
+
+
